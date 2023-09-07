0916a55006d70149947d83ca31934a6640dea018
DanilMargaryan <margaryan.danil@mail.ru>
2023-09-07 13:13:52 +0300

    Added models and configured connection to PostgreSQL

--- web/watchdogs/clubs/models.py
+++ web/watchdogs/clubs/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Club(models.Model):
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.address
+
+    def get_absolute_url(self):
+        return reverse('club', kwargs={'club_id': self.pk})
+
+    class Meta:
+        verbose_name = 'компьютерные клубы'
+        verbose_name_plural = 'компьютерные клубы'
+        ordering = ['address']
+
+
+class Camera(models.Model):
+    ip = models.CharField(max_length=200)
+    location = models.CharField(max_length=150, null=True, blank=True)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ip
+
+    class Meta:
+        verbose_name = 'камера видеонаблюдения'
+        verbose_name_plural = 'камеры видеонаблюдения'
+        ordering = ['club', 'ip']
+
+
+class Picture(models.Model):
+    photo = models.ImageField(upload_to="photos/")
+    time = models.DateTimeField(auto_now_add=True)
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
--- web/watchdogs/watchdogs/settings.py
+++ web/watchdogs/watchdogs/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clubs.apps.ClubsConfig',
+    'clubs.apps.ClubsConfig'
 ]
 
 MIDDLEWARE = [
@@ -70,17 +68,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'watchdogs.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'watchdog',
+        'USER': 'Danil',
+        'PASSWORD': '1212',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -100,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -112,7 +110,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -122,3 +119,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
