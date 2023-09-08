89af1bba6682b195545c61b7d532f3a6cce40cee
DanilMargaryan <margaryan.danil@mail.ru>
2023-09-08 03:46:46 +0300

    add clubs section

--- web/watchdogs/clubs/urls.py
+++ web/watchdogs/clubs/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
+    path('club/<int:club_id>/', show_club, name='club'),
 ]
--- web/watchdogs/clubs/views.py
+++ web/watchdogs/clubs/views.py
@@ -17,5 +17,18 @@ def index(request):
     # return render(request, 'clubs/index.html')
 
 
+def show_club(request, club_id):
+    club = Club.objects.all()
+    pictures = Picture.objects.filter(camera__club_id=club_id).order_by('-time')
+    context = {
+        'club': club,
+        'picture': pictures,
+        'title': 'клубы',
+        'club_selected': club_id,
+    }
+
+    return render(request, 'clubs/club.html', context=context)
+
+
 def page_not_found(request, exception):
     return HttpResponseNotFound("<h1>Страница не найдена</h1>")
