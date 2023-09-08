da1b908b79010df1c00541f174d0416d9e682ff2
DanilMargaryan <margaryan.danil@mail.ru>
2023-09-08 03:31:36 +0300

    add main page

--- web/watchdogs/clubs/views.py
+++ web/watchdogs/clubs/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+from clubs.models import Club, Picture
+
+
+def index(request):
+    club = Club.objects.all()
+
+    context = {
+        'club': club,
+        'title': 'Главная страница',
+        'club_selected': 0,
+    }
+
+    return render(request, 'clubs/index.html', context=context)
+    # return render(request, 'clubs/index.html')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
--- web/watchdogs/watchdogs/settings.py
+++ web/watchdogs/watchdogs/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-1+-x@!ea2gxy*k*scw@(lourcps&v15sbd_l9-bq7@jfdan1@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -113,7 +113,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
